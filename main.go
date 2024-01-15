e6f1ff0047eb0534c6e2d20c3b8e9dfa6304dfae
Marcus Mesta <Marcusmesta@Protonmail.com>
2024-01-15 10:51:01 -0800

    linting

--- main.go
+++ main.go
@@ -6,23 +6,17 @@ import (
 )
 
 func init() {
-
 	//InputFile = CurrentCapDevice.PromptCMDInputSelect()  // CLI mode
 	gui.InitWindowReources(resourceQuarternotePng, resourcePlayiconPng, resourcePauseiconPng)
 
 	// initial metronome core
 	go tempo.MainMetronome.StartMetronome()
-
 }
 
 func main() {
 	// start input select monitor
 	tempo.CreateNewDevMonitor()
+
 	// show and run fyne app
 	gui.TempgoFyneApp.FyneWindow.ShowAndRun()
-
-	// time consuming but possible features
-	// todo need another way to give user permission to input without allowing all userspace to access
-	// todo
-	// need to embed images into app
 }
